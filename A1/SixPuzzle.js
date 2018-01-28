46aac661bd65bde720b7022117db1d2c46303fa4
erick <erick@hotmail.ca>
2018-01-28 15:44:49 -0500

    Add ability to move around puzzle state

--- A1/SixPuzzle.js
+++ A1/SixPuzzle.js
@@ -1,9 +1,11 @@
 class SixPuzzle {
 
 
-  constructor() {
-    this.state = [1,4,2,5,3,'E'];
+  constructor(initial, goal) {
+    this.state = initial;
+    this.goal = goal;
     this.WIDTH = 3;
+    this.HEIGHT = 2;
   }
 
   getCoordinatesFromIndex(i) {
@@ -12,6 +14,31 @@ class SixPuzzle {
 
   getIndexFromCoordinates(i,j) {
     return j+i*3;
+  }
+
+  getAdjacent(i) {
+    const adjIndexes = [];
+    const iPos = this.getCoordinatesFromIndex(i);
+    const isAdjacent = (a, b) => (Math.abs(a[0] - b[0]) === 1 && a[1] === b[1]) || (Math.abs(a[1] - b[1]) === 1 && a[0] === b[0]);
+
+    for (let j = 0; j < this.WIDTH*this.HEIGHT; j++) {
+      const jPos = this.getCoordinatesFromIndex(j);
+      if (isAdjacent(iPos, jPos)) {
+        adjIndexes.push(j);
+      }
+    }
+
+    return adjIndexes;
+  }
+
+  getEmpty() {
+    return this.state.indexOf('E');
+  }
+
+  swap(a, b) {
+    const tmp = this.state[a];
+    this.state[a] = this.state[b];
+    this.state[b] = tmp;
   }
 
   print() {
