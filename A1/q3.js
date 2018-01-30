ace63fce5cefaba582baec3dc508595b43ca7f82
erick <erick@hotmail.ca>
2018-01-29 19:54:21 -0500

    Track number of steps for each hill climb

--- A1/q3.js
+++ A1/q3.js
@@ -1,7 +1,7 @@
 const hillClimb = (startPos, stepSize, minBound, maxBound, fn) => {
-
   let pos = startPos;
   let isMax = false;
+  let numSteps = 0;
 
   while (!isMax) {
     const val = fn(pos);
@@ -13,11 +13,12 @@ const hillClimb = (startPos, stepSize, minBound, maxBound, fn) => {
       isMax = true;
     } else {
       pos = neighbors.find(n => fn(n) === Math.max(...neighborValues));
+      numSteps++;
     }
   }
 
-  console.log(`Start: ${startPos}   Step Size: ${stepSize}`);
-  console.log(pos);
+  console.log(`Start: ${startPos}  `, `Step Size: ${stepSize}`);
+  console.log(`  Max: ${pos}`     , `Max value: ${fn(pos)}`, `Steps: ${numSteps}`);
 
 }
 const stepSizes = Array(10).fill().map((v,i) => (i+1)*0.01);
@@ -30,6 +31,6 @@ const fn = (x) => ( Math.sin(Math.pow(x,2) / 2) / Math.log2(x + 4) );
 
 stepSizes.forEach(size => {
   startingPoints.forEach(pos => {
-  hillClimb(pos, size, MIN_BOUND, MAX_BOUND, fn);
+    hillClimb(pos, size, MIN_BOUND, MAX_BOUND, fn);
   });
 });
