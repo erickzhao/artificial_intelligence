213c81e5291788944cac6b81a10044677c377516
erick <erick@hotmail.ca>
2018-01-29 19:29:08 -0500

    Implement hill-climbing

--- A1/q3.js
+++ A1/q3.js
@@ -1,1 +1,35 @@
-const fn = (x) => (Math.sin((x^2) / 2) / Math.log2(x + 4));
+const hillClimb = (startPos, stepSize, minBound, maxBound, fn) => {
+
+  let pos = startPos;
+  let isMax = false;
+
+  while (!isMax) {
+    const val = fn(pos);
+    const neighbors = [pos + stepSize, pos - stepSize]
+      .filter(n => n >= minBound && n <= maxBound);
+    const neighborValues = neighbors
+      .map(n => fn(n));
+    if (val >= Math.max(...neighborValues)) {
+      isMax = true;
+    } else {
+      pos = neighbors.find(n => fn(n) === Math.max(...neighborValues));
+    }
+  }
+
+  console.log(`Start: ${startPos}   Step Size: ${stepSize}`);
+  console.log(pos);
+
+}
+const stepSizes = Array(10).fill().map((v,i) => (i+1)*0.01);
+const startingPoints = Array(11).fill().map((v,i) => i);
+
+const MIN_BOUND = 0;
+const MAX_BOUND = 10; 
+
+const fn = (x) => ( Math.sin(Math.pow(x,2) / 2) / Math.log2(x + 4) );
+
+stepSizes.forEach(size => {
+  startingPoints.forEach(pos => {
+  hillClimb(pos, size, MIN_BOUND, MAX_BOUND, fn);
+  });
+});
